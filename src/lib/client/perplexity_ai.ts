52999dbcb3402c44ed13580d2eaf17a47e0bc7c7
Daniel Gorbachev <dg186533@gmail.com>
2026-03-15 20:54:51 -0400

    changed the perplexity export client from client to function definition

--- src/lib/client/perplexity_ai.ts
+++ src/lib/client/perplexity_ai.ts
@@ -1,5 +1,7 @@
 import Perplexity from '@perplexity-ai/perplexity_ai'
 
-export const perplexityClient = new Perplexity({
-  apiKey: process.env['PERPLEXITY_API_KEY'], // This is the default and can be omitted
-})
+export function getPerplexityClient() {
+  return new Perplexity({
+    apiKey: process.env.PERPLEXITY_API_KEY,
+  })
+}
